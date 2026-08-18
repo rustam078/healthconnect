496f485088dc24c3cdeb1e334ea163effa8459ea
mustafa-ali-eng <mustafaali8597@gmail.com>
2026-08-18 10:19:24 +0530

    KAN-19 first commit

---
+++ src/main/java/in/mustafa/HealthConnectApplication.java
@@ -0,0 +1,13 @@
+package in.mustafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/mustafa/HealthConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mustafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
