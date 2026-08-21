8eb6e388f82b72e03b014e9a5ec04c6397fed511
mustafa-ali-eng <mustafaali8597@gmail.com>
2026-08-21 16:27:50 +0530

    KAN-40-KAN-41-Implement patient deactivation API

--- src/main/java/in/healthconnect/entity/BaseEntity.java
+++ src/main/java/in/healthconnect/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package in.healthconnect.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,6 +14,7 @@ import java.time.Instant;
 @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@SQLRestriction("is_deleted = false")//Normal Hibernate queries mein is_deleted = true wale patients ko automatically exclude kar dena.
 public abstract class BaseEntity {
 
  @Id
